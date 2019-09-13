4047afbeaae280daac199ca61ce86554fb9bc05e
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2019-09-13 13:32:47 +0700

    first

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int TIME = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(),Home.class));
+            finish();
+        }, TIME);
+    }
+}
